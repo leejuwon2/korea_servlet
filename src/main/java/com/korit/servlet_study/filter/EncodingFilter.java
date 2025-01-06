5c73632038a7a1f9fa6cbdd1a6ab08afcef80bb8
code1218 <skjil1218@gmail.com>
2025-01-06 16:34:44 +0900

    serach

--- src/main/java/com/korit/servlet_study/filter/EncodingFilter.java
+++ src/main/java/com/korit/servlet_study/filter/EncodingFilter.java
@@ -2,13 +2,23 @@ package com.korit.servlet_study.filter;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @WebFilter("*")
 public class EncodingFilter implements Filter {
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("필터 동작!!");
+        System.out.println("필터!!");
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        servletRequest.setCharacterEncoding("UTF-8");
+        servletResponse.setCharacterEncoding("UTF-8");
+
+        filterChain.doFilter(servletRequest, servletResponse);
+
+        System.out.println("후처리");
     }
 
     @Override
--- src/main/java/com/korit/servlet_study/servlet/ProductRegisterPageServlet.java
+++ src/main/java/com/korit/servlet_study/servlet/ProductRegisterPageServlet.java
@@ -24,6 +24,7 @@ public class ProductRegisterPageServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("상품으록 페이지 doGet 호출");
         HttpSession session = req.getSession();
         session.setAttribute("username", "junil");
 
@@ -33,17 +34,18 @@ public class ProductRegisterPageServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setCharacterEncoding("UTF-8");
         System.out.println(req.getParameter("category"));
         System.out.println(req.getParameter("productName"));
         System.out.println(req.getParameter("price"));
         System.out.println(req.getParameter("registerDate"));
-        resp.setContentType("text/html;charset=UTF-8");
-        resp.getWriter().println(
-                "<script>"
-                        + "alert(\"등록이 완료되었습니다.\");"
-                        + "location.href='http://localhost:8080/servlet_study_war/product/register';"
-                        + "</script>"
-        );
+        resp.setContentType("application/json");
+        resp.setStatus(200);
+        resp.getWriter().println("{\"name\":\"김준일\"}");
+//        resp.getWriter().println(
+//                "<script>"
+//                        + "alert(\"등록이 완료되었습니다.\");"
+//                        + "location.href='http://localhost:8080/servlet_study_war/product/register';"
+//                        + "</script>"
+//        );
     }
 }
